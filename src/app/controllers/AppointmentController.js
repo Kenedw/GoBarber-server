1aa998ae0438a1704954172360ce3c9d7a2cb81f
Kened Oliveira <kenedwanderson@live.com>
2019-09-01 18:53:39 -0300

    feat/create appointment

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,72 @@
+import * as yup from 'yup';
+import { startOfHour, parseISO, isBefore, addHours } from 'date-fns';
+import { Op } from 'sequelize';
+
+import User from '../models/Users';
+import Appointment from '../models/Appointment';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = yup.object().shape({
+      provider_id: yup.number().required(),
+      date: yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    /**
+     * check if provider_id is a provider
+     */
+    const checkIsProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ error: 'You can only create appointments with providers' });
+    }
+
+    /**
+     * check for past dates
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted.' });
+    }
+
+    /**
+     * check date availability, one appointment per hour
+     */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: {
+          [Op.between]: [date, addHours(new Date(date), 1)],
+        },
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json({ appointment });
+  }
+}
+
+export default new AppointmentController();
