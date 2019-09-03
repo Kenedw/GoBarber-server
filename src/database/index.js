3350f5fdd2bf3fcd7d52f6c3657c3e205c391b34
Kened Oliveira <kenedwanderson@live.com>
2019-09-02 23:31:13 -0300

    chore/ connect with mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import databaseConfig from '../config/database';
 
@@ -10,15 +11,23 @@ const models = [User, File, Appointment];
 
 class Database {
   constructor() {
-    this.init();
+    this.postgres();
+    this.mongo();
   }
 
-  init() {
+  postgres() {
     this.connection = new Sequelize(databaseConfig);
 
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://192.168.99.100:27017/gobarber',
+      { useNewUrlParser: true, useFindAndModify: true }
+    );
   }
 }
 
