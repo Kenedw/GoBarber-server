f569926428943e2623b2e55a5923ab72c149c757
Kened Oliveira <kenedwanderson@live.com>
2019-09-03 23:28:39 -0300

    feat/ send email when cancel an appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -14,6 +14,7 @@ import User from '../models/Users';
 import Appointment from '../models/Appointment';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
   async index(req, res) {
@@ -135,6 +136,13 @@ class AppointmentController {
   async delete(req, res) {
     const appointment = await Appointment.findOne({
       where: { id: req.params.id, canceled_at: null },
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
     });
 
     if (appointment === null) {
@@ -157,6 +165,12 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      text: 'Você tem um novo cancelamento.',
+    });
 
     return res.json({ appointment });
   }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '79c5bae9ff115c',
+    pass: '0ac05fa1bcb254',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
