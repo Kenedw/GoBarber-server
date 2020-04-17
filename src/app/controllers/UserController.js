77fb25a8d788af62c61c68e2f72b78f56b83b7a0
Kened Oliveira <kenedwanderson@live.com>
2020-04-16 22:27:36 -0300

    fix: include avatar

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,7 @@
 import * as Yup from 'yup';
 
 import User from '../models/Users';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -60,9 +61,19 @@ class UserController {
       return res.status(401).json({ error: 'Password does not math' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    await user.update(req.body);
 
-    return res.json({ id, name, email, provider });
+    const { id, name, provider, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
+
+    return res.json({ id, name, email, provider, avatar });
   }
 }
 
